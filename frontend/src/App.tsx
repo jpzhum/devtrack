b63b4653567459ac3a957b7fb0acd6e09f68ed4f
kel <kelzinhasky12@gmail.com>
2026-08-05 23:17:57 -0300

    feat: initialize frontend with React and Vite

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import WelcomeMessage from './assets/componentes/WelcomeMessage'
+
+function App() {
+  return (
+    <main> 
+      <h1>devtrack</h1>
+      <p>acompanhe sua evolução como desenvolvedora</p>
+
+      <WelcomeMessage />
+    </main>
+  )
+}
+
+export default App
---
+++ frontend/src/assets/componentes/WelcomeMessage.tsx
@@ -0,0 +1,10 @@
+function WelcomeMessage() {
+  return (
+    <section>
+        <h2>bem-vinda ao devtrack :D</h2>
+        <p>seu progresso comeca aqui!</p>
+    </section>
+  )
+}
+
+export default WelcomeMessage 
