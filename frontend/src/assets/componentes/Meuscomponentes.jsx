51b12f2c570d780fb3d378f1c0cfa5e0de16cb5b
kel <kelzinhasky12@gmail.com>
2026-08-06 12:52:15 -0300

    feat: initialize frontend with React and Vite

---
+++ frontend/src/assets/componentes/Meuscomponentes.jsx
@@ -0,0 +1,13 @@
+import './Meuscomponentes.jsx'
+import welcomemessage from './components/welcomemessage'
+
+function MeusComponentes() {
+    return (
+        <section>
+            <h1>meus componentes</h1>
+            <p>teste :D</p>
+        </section>
+    );
+}
+
+export default MeusComponentes
